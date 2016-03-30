5a8a9e11dff8aefbaf74724f6580df441851a97b
hongpengfei <hongpengfei@yimei180.com>
2016-03-30 13:04:20 +0800

    1

---
+++ app/src/main/java/App.java
@@ -0,0 +1,8 @@
+/**
+ * Created by hongpf on 16/3/30.
+ */
+public class App {
+    public static void main(String [] args  ){
+        System.out.println("appRunting");
+    }
+}
---
+++ src/main/java/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * Created by hongpf on 16/3/30.
+ */
+public class Hello {
+    public static void main(String [] args ){
+        System.out.println("sdfsdfdsfsddfdsfsd");
+    }
+}
